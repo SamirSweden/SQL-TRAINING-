5328a05db0f77c70be01b1e68587b5a7adc33f19
Samir Azimov <samirazimov488@gmail.com>
2025-12-08 04:56:57 +0500

    Update JOINS.sql

--- JOINS.sql
+++ JOINS.sql
@@ -7,7 +7,13 @@ JOIN Person.Person p ON p.BusinessEntityID = e.BusinessEntityID
 ORDER BY p.FirstName,p.LastName -- Получить имя, фамилию и должность всех сотрудников
 
 
-
+SELECT e.BusinessEntityID , p.FirstName , p.LastName , e.BirthDate 
+FROM HumanResources.Employee e
+JOIN Person.Person p ON e.BusinessEntityID = p.BusinessEntityID
+WHERE e.BirthDate = (
+	SELECT MAX(BirthDate)
+	FROM HumanResources.Employee
+)
 
 
 
